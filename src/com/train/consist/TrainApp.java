22e82b10dd4ff59e1cc8ba5db219b46d77c8b91c
aniviyaksivarajan2005-coder <aniviyaksivarajan2005@gmail.com>
2026-04-09 15:13:26 +0530

    UC2 Added bogies and display train composition

--- src/com/train/consist/TrainApp.java
+++ src/com/train/consist/TrainApp.java
@@ -7,26 +7,24 @@ public class TrainApp {
 
     public static void main(String[] args) {
 
-        // Step 1: Welcome Message
+        // UC1: Initialization
         System.out.println("=== Train Consist Management App ===");
 
-        // Step 2: Initialize Train Consist
         List<String> trainConsist = new ArrayList<>();
 
         System.out.println("Train initialized successfully.");
         System.out.println("Initial bogie count: " + trainConsist.size());
 
-        // Step 3: Add Bogies
+        // UC2: Add Bogies
         trainConsist.add("Engine");
         trainConsist.add("Sleeper Coach");
         trainConsist.add("General Coach");
         trainConsist.add("AC Coach");
 
-        // Step 4: Display Updated Count
         System.out.println("\nBogies added successfully.");
         System.out.println("Total bogie count: " + trainConsist.size());
 
-        // Step 5: Display Train Consist
+        // Display Train
         System.out.println("\nCurrent Train Composition:");
         for (String bogie : trainConsist) {
             System.out.println(bogie);
